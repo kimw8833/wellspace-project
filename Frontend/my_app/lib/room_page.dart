2c49be957d502a03e1966f1cda3e90e3b4078eff
martingundhus <martin.gundhus.1072@student.uu.se>
2025-12-03 10:49:48 +0100

    print roomMood and dogMood

--- Frontend/my_app/lib/room_page.dart
+++ Frontend/my_app/lib/room_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'api_service.dart';
 
 class MyRoomPage extends StatefulWidget {
   final int playerId;
@@ -9,15 +10,63 @@ class MyRoomPage extends StatefulWidget {
 }
 
 class _MyRoomPageState extends State<MyRoomPage> {
+  late Future<int?> roomMood;
+  late Future<int?> dogMood;
+
+  @override
+  void initState() {
+    super.initState();
+    roomMood = ApiService().getRoomMood(widget.playerId);
+    dogMood = ApiService().getDogStatus(widget.playerId);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/roomHappy.png"),
-          //fit: BoxFit.cover,
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/roomHappy.png"),
+              //fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          top: 50,
+          left: 30,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FutureBuilder<int?>(
+                future: roomMood,
+                builder: (context, snapshot) {
+                  return Text(
+                    "Room mood: ${snapshot.data}",
+                    style: const TextStyle(
+                      fontSize: 28,
+                      color: Colors.white,
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(height: 20),
+              FutureBuilder<int?>(
+                future: dogMood,
+                builder: (context, snapshot) {
+                  return Text(
+                    "Dog mood: ${snapshot.data}",
+                    style: const TextStyle(
+                      fontSize: 28,
+                      color: Colors.white,
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
